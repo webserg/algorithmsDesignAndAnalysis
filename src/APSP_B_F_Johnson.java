53463ff98604690a3b430feca474d559a435f83f
webserg@gmail.com <webserg@gmail.com>
2014-07-02 14:30:07 +0300

    v1

---
+++ src/APSP_B_F_Johnson.java
@@ -0,0 +1,138 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by webserg on 02.07.2014.
+ * All pair shortest path using Johnson(BellMan-Ford for detecting negative circles then weighting then Dijkstra )
+ * large graph
+ */
+public class APSP_B_F_Johnson {
+    static final long INFINITY = Long.MAX_VALUE;
+    static final long MIN_INFINITY = Long.MIN_VALUE;
+
+    long run(Graph graph) {
+        long res = INFINITY;
+        int N = graph.vertexes.length;
+        long[] D = new long[N];
+        for (int i = 0; i < N; i++) {
+            D[i] = INFINITY;
+        }
+        D[1] = 0;
+        for (int k = 1; k < N; k++) {
+            for (Edge edge : graph.edges) {
+                if ( D[edge.u] + edge.w < D[edge.v]) {
+                    D[edge.v] = D[edge.u] + edge.w;
+                    if (k == N - 1) {
+                        System.out.println("negative cycle");
+                        return MIN_INFINITY;
+                    }
+                }
+            }
+        }
+
+
+        return D[N-1];
+    }
+
+    @Test
+    public void testAPSPgTest2() throws Exception {
+        Graph graph = readGraphFromFile("resource/gTest2.txt");
+        Assert.assertEquals(-7, run(graph));
+        System.out.println("");
+    }
+
+    @Test
+    public void testBellmanFord() throws Exception {
+        Graph graph = readGraphFromFile("resource/bellmanFordTest.txt");
+        Assert.assertEquals(7, run(graph));
+        System.out.println("");
+    }
+
+
+    @Test
+    public void testBellmanFord2() throws Exception {
+        Graph graph = readGraphFromFile("resource/bellmanFordTest2.txt");
+        Assert.assertEquals(7, run(graph));
+        System.out.println("");
+    }
+
+
+    @Test
+    public void testAPSPLarge() throws Exception {
+        Graph graph = readGraphFromFile("resource/large.txt");
+    }
+
+    static class Vertex {
+        private int v;
+        int score;
+
+        Vertex(int v) {
+            this.v = v;
+        }
+    }
+
+    static class Edge {
+        private int u;
+        private int v;
+        private long w;
+
+        Edge(int u, int v, long w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+    }
+
+    static class Graph {
+        Vertex[] vertexes;
+        Edge[] edges;
+
+        Graph(Vertex[] vertexes, Edge[] edges) {
+            this.vertexes = vertexes;
+            this.edges = edges;
+        }
+    }
+
+    static Graph readGraphFromFile(String filePath) {
+        Charset charset = Charset.forName("UTF-8");
+        Path path = Paths.get(filePath);
+        Vertex[] vertexes = null;
+        Edge[] edges = null;
+        int N;
+        try (BufferedReader reader = Files.newBufferedReader(path, charset)) {
+            String line = reader.readLine();
+            String[] strLine = line.split("\\s+");
+            N = Integer.parseInt(strLine[0]);
+            int edgesNum = Integer.parseInt(strLine[1]);
+            vertexes = new Vertex[N + 1];
+            edges = new Edge[edgesNum];
+
+            int i, j;//idx of vertexes
+            int w;//weight
+            int e = 0;
+            while ((line = reader.readLine()) != null) {
+                strLine = line.split("\\s+");
+                i = Integer.parseInt(strLine[0]);
+                j = Integer.parseInt(strLine[1]);
+                w = Integer.parseInt(strLine[2]);
+                edges[e++] = new Edge(i, j, w);
+                if (vertexes[i] == null) {
+                    vertexes[i] = new Vertex(i);
+                }
+            }
+        } catch (IOException x) {
+            System.out.println("IOException:" + x.getMessage());
+        }
+
+        return new Graph(vertexes, edges);
+    }
+
+
+}
