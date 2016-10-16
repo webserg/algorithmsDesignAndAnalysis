1ed5a0a10e15d0e81ad7412af6e89b16a331ccc9
webserg <webserg@gmail.com>
2016-10-16 11:35:13 +0300

    init2

--- src/MergeSortInversionsCount.java
+++ src/MergeSortInversionsCount.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * User: webserg
  * Date: 12.02.13
@@ -13,54 +15,45 @@ import org.junit.Test;
  * inversions = number of pair[i,j] of array indices with i < j and A[i] > A[j]
  */
 public class MergeSortInversionsCount {
-    private static int[] source;
-    private static long inversions;
+    private static int[] source2;
+    private static long inversions2;
 
     @Test
     public void testMergeSort() throws Exception {
-        System.out.println("start...");
-        source = FilesHelper.readIntArrayFromFile("resource/IntegerArray.txt", 100000);
-        mergeSort(source);
-        FilesHelper.writeIntArrayToFile("resource/out.txt", source);
-        FilesHelper.writeLongToFile("resource/inversions.txt", inversions);
-        System.out.println("inversions = " + inversions);
-        Assert.assertEquals(2407905288L, inversions);
+        source2 = new int[]{2,3,4,1,5,6};
+        source2 = FilesHelper.readIntArrayFromFile("resource/IntegerArray2.txt", 100000);
+        mergeSort(source2,0,source2.length-1);
+        System.out.println("source2 = " + Arrays.toString(source2));
+        System.out.println("inversions2 = " + inversions2);
     }
 
-    public void mergeSort(int[] s) throws Exception {
-        source = s;
-        if (source.length < 2)
-            return;
-        split(0, s.length - 1);
-
-    }
-
-    private static void split(final int first, final int last) {
-        if (first < last) {
-            int middle = (first + last) >>> 1;
-            split(first, middle);
-            if (first < last) split(middle + 1, last);
-            merge(first, middle, middle + 1, last);
+    private static void mergeSort(int[] array, final int i, final int j) {
+        if (i < j) {
+            int m = (i + j) >>> 1;
+            mergeSort(array, i, m);
+            mergeSort(array, m + 1, j);
+            merge(array, i, m, j);
         }
     }
 
-    private static void merge(int fl, int ll, int fr, int lr) {
-        int[] temp = new int[source.length];
-        int tmp_idx = 0;
-        int saveF = fl;
-        while (fl <= ll && fr <= lr) {
-            if (source[fl] < source[fr]) {
-                temp[tmp_idx] = source[fl];
-                fl++;
+    private static void merge(int[] array, final int i, final int m, final int j) {
+        int kk = i;
+        int left[] = Arrays.copyOfRange(array, i, m + 1);
+        int right[] = Arrays.copyOfRange(array, m + 1, j + 1);
+        int leftK = 0, rightK = 0;
+        for (; kk < j && leftK < left.length && rightK < right.length; kk++) {
+            if (left[leftK] < right[rightK]) {
+                array[kk] = left[leftK++];
             } else {
-                temp[tmp_idx] = source[fr];
-                fr++;
-                inversions += (ll + 1 - fl);
+                array[kk] = right[rightK++];
+                inversions2 += (left.length - leftK);
             }
-            tmp_idx++;
         }
-        while (fl <= ll) temp[tmp_idx++] = source[fl++];
-        while (fr <= lr) temp[tmp_idx++] = source[fr++];
-        System.arraycopy(temp, saveF, source, saveF, lr - saveF + 1);
+        while (leftK < left.length && kk <= j) {
+            array[kk++] = left[leftK++];
+        }
+        while (rightK < right.length && kk <= j) {
+            array[kk++] = right[rightK++];
+        }
     }
 }
