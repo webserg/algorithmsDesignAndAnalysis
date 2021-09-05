9b35983aa668eb2e56203e12f815816cab0f5c1c
webserg <webserg@gmail.com>
2021-09-05 18:37:00 +0300

    reorginize and add merge sort

--- src/com/gmail/webserg/algorithmsAndAnalysis/leetCode/BestTimeToBuyAndSellStock.java
+++ src/com/gmail/webserg/algorithmsAndAnalysis/leetCode/BestTimeToBuyAndSellStock.java
@@ -1,4 +1,44 @@
 package com.gmail.webserg.algorithmsAndAnalysis.leetCode;
 
+/**
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ * <p>
+ * On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+ * <p>
+ * Find and return the maximum profit you can achieve.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+ * Total profit is 4 + 3 = 7.
+ * Example 2:
+ * <p>
+ * Input: prices = [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+ * Total profit is 4.
+ * Example 3:
+ * <p>
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of
+ */
 public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            profit += Math.max(prices[i + 1] - prices[i], 0);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{1,2,3,4,5}));
+    }
 }
--- src/com/gmail/webserg/algorithmsAndAnalysis/leetCode/RotateArray.java
+++ src/com/gmail/webserg/algorithmsAndAnalysis/leetCode/RotateArray.java
@@ -13,10 +13,14 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
  */
 public class RotateArray {
-    static int arr[] = new int[]{1, 2, 3, 4, 5};
+
+    static public void rotate(int[] nums, int k) {
+        for (int i = 0; i < k; i++)
+            rotate2(nums);
+    }
 
 
-    static void rotate() {
+    static void rotate1(int[] arr) {
         int i = 0, j = arr.length - 1;
         while (i != j) {
             int temp = arr[i];
@@ -26,14 +30,48 @@ public class RotateArray {
         }
     }
 
+    static void rotate2(int[] arr) {
+        int i, temp;
+        temp = arr[0];
+        int n = arr.length;
+        for (i = 0; i < n - 1; i++)
+            arr[i] = arr[i + 1];
+        arr[n - 1] = temp;
+    }
+
     /* Driver program */
     public static void main(String[] args) {
         System.out.println("Given Array is");
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
         System.out.println(Arrays.toString(arr));
-
-        rotate();
+        rotate1(arr);
 
         System.out.println("Rotated Array is");
         System.out.println(Arrays.toString(arr));
+
+        System.out.println("Given Array is");
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(arr));
+        rotate2(arr);
+
+        System.out.println("Rotated Array is");
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Given Array is");
+        arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        System.out.println(Arrays.toString(arr));
+        rotate(arr, 3);
+
+        System.out.println("Rotated Array is");
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Given Array is");
+        arr = new int[]{-1, -100, 3, 99};
+        System.out.println(Arrays.toString(arr));
+        rotate(arr, 2);
+
+        System.out.println("Rotated Array is");
+        System.out.println(Arrays.toString(arr));
+
     }
 }
